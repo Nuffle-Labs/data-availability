ff516647524abca039897c24bf19951a8d4764fd
Jacob Lindahl <encody@users.noreply.github.com>
2023-11-03 19:16:37 +0900

    chore: adds PartialEq derives for http structs

--- crates/http-api-data/src/lib.rs
+++ crates/http-api-data/src/lib.rs
@@ -6,7 +6,7 @@ use alloc::vec::Vec;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct ConfigureClientRequest {
     pub account_id: String,
     pub secret_key: String,
@@ -15,25 +15,25 @@ pub struct ConfigureClientRequest {
     pub namespace: Namespace,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct Namespace {
     pub version: u8,
     pub id: u32,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct BlobRequest {
     #[serde(rename = "tx")]
     pub transaction_id: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct SubmitRequest {
     pub blobs: Vec<Blob>,
 }
 
 #[serde_with::serde_as]
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct Blob {
     pub namespace: Namespace,
     pub share_version: u32,

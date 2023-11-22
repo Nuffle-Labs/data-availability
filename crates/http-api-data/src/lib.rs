560ace8f564b473b8003310372885874d9814698
Jacob Lindahl <encody@users.noreply.github.com>
2023-11-22 23:43:25 +0900

    chore: don't reimplement primitives in http-api-data

--- crates/http-api-data/src/lib.rs
+++ crates/http-api-data/src/lib.rs
@@ -4,6 +4,7 @@ extern crate alloc;
 use alloc::string::String;
 use alloc::vec::Vec;
 
+pub use near_da_primitives::{Blob, Namespace};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
@@ -16,12 +17,6 @@ pub struct ConfigureClientRequest {
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
-pub struct Namespace {
-    pub version: u8,
-    pub id: u32,
-}
-
-#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct BlobRequest {
     #[serde(rename = "tx")]
     pub transaction_id: String,
@@ -30,17 +25,6 @@ pub struct BlobRequest {
 #[serde_with::serde_as]
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub struct SubmitRequest {
-    #[serde_as(as = "serde_with::hex::Hex")]
-    pub data: Vec<u8>,
-}
-
-#[serde_with::serde_as]
-#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
-pub struct Blob {
-    pub namespace: Namespace,
-    pub share_version: u32,
-    #[serde_as(as = "serde_with::hex::Hex")]
-    pub commitment: [u8; 32],
     #[serde_as(as = "serde_with::hex::Hex")]
     pub data: Vec<u8>,
 }

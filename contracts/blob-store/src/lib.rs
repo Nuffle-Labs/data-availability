3c0ca51419b0d5375dcba71b010ad2c643c58568
Jacob Lindahl <encody@users.noreply.github.com>
2023-11-23 23:17:10 +0900

    chore: use const slice ref for storage keys

--- contracts/blob-store/src/lib.rs
+++ contracts/blob-store/src/lib.rs
@@ -2,16 +2,6 @@
 
 use near_sdk::{assert_one_yocto, env, AccountId};
 
-#[no_mangle]
-pub fn submit() {
-    require_initialized();
-    require_owner(&env::predecessor_account_id());
-
-    if env::input().is_none() {
-        env::panic_str(ERR_MISSING_INVALID_INPUT);
-    }
-}
-
 const ERR_CONTRACT_NOT_INITIALIZED: &str = "Contract is not initialized.";
 const ERR_CONTRACT_ALREADY_INITIALIZED: &str = "Contract already initialized.";
 const ERR_NOT_OWNER: &str = "Predecessor is not owner.";
@@ -20,28 +10,19 @@ const ERR_NOT_PROPOSED_OWNER: &str = "Predecessor is not proposed owner.";
 const ERR_MISSING_INVALID_INPUT: &str = "Missing or invalid input.";
 const JSON_NULL: &[u8] = b"null";
 const JSON_DOUBLE_QUOTE: &[u8] = b"\"";
-
-#[repr(u8)]
-enum StorageKey {
-    Initialized,
-    Owner,         // serialized with .as_bytes() NOT Borsh
-    ProposedOwner, // ditto. Not guaranteed to be a valid AccountId.
-}
-
-macro_rules! key {
-    ($i: ident) => {
-        core::slice::from_ref(&(StorageKey::$i as u8))
-    };
-}
+// storage keys
+const KEY_INITIALIZED: &[u8; 1] = &[0];
+const KEY_OWNER: &[u8; 1] = &[1]; //            serialized with .as_bytes() NOT Borsh
+const KEY_PROPOSED_OWNER: &[u8; 1] = &[2]; //   ditto. Not guaranteed to be a valid AccountId.
 
 fn require_initialized() {
-    if !env::storage_has_key(key!(Initialized)) {
+    if !env::storage_has_key(KEY_INITIALIZED) {
         env::panic_str(ERR_CONTRACT_NOT_INITIALIZED);
     }
 }
 
 fn require_owner(predecessor: &AccountId) {
-    if env::storage_read(key!(Owner))
+    if env::storage_read(KEY_OWNER)
         .filter(|v| v == predecessor.as_bytes())
         .is_none()
     {
@@ -51,15 +32,25 @@ fn require_owner(predecessor: &AccountId) {
 
 #[no_mangle]
 pub fn new() {
-    if env::storage_has_key(key!(Initialized)) {
+    if env::storage_has_key(KEY_INITIALIZED) {
         env::panic_str(ERR_CONTRACT_ALREADY_INITIALIZED);
     }
 
-    env::storage_write(key!(Initialized), &[1]);
+    env::storage_write(KEY_INITIALIZED, &[1]);
 
     let predecessor_account_id = env::predecessor_account_id();
 
-    env::storage_write(key!(Owner), predecessor_account_id.as_bytes());
+    env::storage_write(KEY_OWNER, predecessor_account_id.as_bytes());
+}
+
+#[no_mangle]
+pub fn submit() {
+    require_initialized();
+    require_owner(&env::predecessor_account_id());
+
+    if env::input().is_none() {
+        env::panic_str(ERR_MISSING_INVALID_INPUT);
+    }
 }
 
 fn return_json_string(v: Option<&[u8]>) {
@@ -74,7 +65,7 @@ fn return_json_string(v: Option<&[u8]>) {
 pub fn own_get_owner() {
     require_initialized();
 
-    let current_owner = env::storage_read(key!(Owner));
+    let current_owner = env::storage_read(KEY_OWNER);
 
     return_json_string(current_owner.as_deref());
 }
@@ -83,7 +74,7 @@ pub fn own_get_owner() {
 pub fn own_get_proposed_owner() {
     require_initialized();
 
-    let current_proposed_owner = env::storage_read(key!(ProposedOwner));
+    let current_proposed_owner = env::storage_read(KEY_PROPOSED_OWNER);
 
     return_json_string(current_proposed_owner.as_deref());
 }
@@ -110,10 +101,10 @@ pub fn own_propose_owner() {
 
     match new_proposed_owner {
         Some(new_proposed_owner) => {
-            env::storage_write(key!(ProposedOwner), new_proposed_owner);
+            env::storage_write(KEY_PROPOSED_OWNER, new_proposed_owner);
         }
         None => {
-            env::storage_remove(key!(ProposedOwner));
+            env::storage_remove(KEY_PROPOSED_OWNER);
         }
     }
 }
@@ -123,15 +114,15 @@ pub fn own_accept_owner() {
     require_initialized();
     assert_one_yocto();
     let predecessor = env::predecessor_account_id();
-    let current_proposed_owner = env::storage_read(key!(ProposedOwner))
+    let current_proposed_owner = env::storage_read(KEY_PROPOSED_OWNER)
         .unwrap_or_else(|| env::panic_str(ERR_NO_PROPOSED_OWNER));
 
     if predecessor.as_bytes() != current_proposed_owner {
         env::panic_str(ERR_NOT_PROPOSED_OWNER);
     }
 
-    env::storage_remove(key!(ProposedOwner));
-    env::storage_write(key!(Owner), &current_proposed_owner);
+    env::storage_remove(KEY_PROPOSED_OWNER);
+    env::storage_write(KEY_OWNER, &current_proposed_owner);
 }
 
 #[no_mangle]
@@ -141,6 +132,6 @@ pub fn own_renounce_owner() {
     let predecessor = env::predecessor_account_id();
     require_owner(&predecessor);
 
-    env::storage_remove(key!(Owner));
-    env::storage_remove(key!(ProposedOwner));
+    env::storage_remove(KEY_OWNER);
+    env::storage_remove(KEY_PROPOSED_OWNER);
 }

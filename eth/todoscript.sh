c3f24ebe18be3c2bf50b49eafe3e60dedd1ef0a6
dndll <donovan@near.org>
2024-04-18 11:21:38 +0100

    ci: actions and cleanup

--- eth/todoscript.sh
+++
@@ -1,2 +0,0 @@
-NEWCONTRACT=forge script Deploy --fork-url local --broadcast --legacy --json | jq -R 'fromjson?' | jq -r '.returns.da.value'
-docker exec zkevm-sequence-sender /app/zkevm-node set-dap --da-addr $NEWCONTRACT --network custom --custom-network-file /app/genesis.json --key-store-path /pk/sequencer.keystore --pw testonly --cfg /app/config.toml
